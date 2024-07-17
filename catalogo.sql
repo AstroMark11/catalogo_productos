28fba037d26eb2d3d191c3197cf714a7a177c683
Mark Castillo <18338175+AstroMark11@users.noreply.github.com>
2024-07-17 12:06:08 -0500

    Update catalogo.sql

--- catalogo.sql
+++ catalogo.sql
@@ -1,2 +1,17 @@
 -- Crear la base de datos catalogoproductos
 create database catalogoproductos;
+
+use catalogoproductos;
+
+-- Crear tabla productos
+create table productos (
+	producto_id int primary key auto_increment,
+    nombre_producto varchar(50),
+    categoría varchar(50),
+    precio decimal(10, 2),
+    descripción text,
+    cantidad_en_existencia int,
+    estado enum ('disponible', 'agotado', 'descontinuado'),
+    fecha_creación timestamp default current_timestamp,
+    fecha_actualización timestamp default current_timestamp 
+);
